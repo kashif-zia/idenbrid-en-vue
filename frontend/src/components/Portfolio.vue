cf57898533af1febe3ef8bfe9c60cd4021fd106e
Umar Idenbrid <azeem.idenbrid@gmail.com>
2023-03-23 15:56:52 +0500

    mixitup changes

--- frontend/src/components/Portfolio.vue
+++ frontend/src/components/Portfolio.vue
@@ -1,5 +1,5 @@
 <template>
-  <div>    
+  <div>
     <PageHeader title="PORTFOLIO" />
     <section class="gallery-section">
       <div class="auto-container">
@@ -10,35 +10,53 @@
               <h2>work showcase</h2>
             </div>
             <!--Filter-->
-            <div class="filters clearfix ">
+            <div class="filters clearfix">
               <div class="filter-tabs filter-btns clearfix">
-                <li type="button" class="filter " data-filter="all">All</li>
-                <li type="button" class="filter" data-filter=".wedevelopment">Web development</li>
-                <li type="button" class="filter" data-filter=".webdesign">Web Design</li>
-                <li type="button" class="filter" data-filter=".ui">UI/UX Design</li>
-                <li type="button" class="filter" data-filter=".webapplication">Web Application</li>
+                <li ref="all" type="button" class="filter" data-filter="all">
+                  All
+                </li>
+                <li type="button" class="filter" data-filter=".wedevelopment">
+                  Web development
+                </li>
+                <li type="button" class="filter" data-filter=".webdesign">
+                  Web Design
+                </li>
+                <li type="button" class="filter" data-filter=".ui">
+                  UI/UX Design
+                </li>
+                <li type="button" class="filter" data-filter=".webapplication">
+                  Web Application
+                </li>
               </div>
             </div>
           </div>
-          <ul class="container auto-container">
+          <ul ref="container" class="container auto-container">
             <div class="mixitup-gallery">
               <div class="row">
                 <!-- Gallery Item -->
-                <li class="gallery-item mix wedevelopment col-lg-4 col-md-6 col-sm-12">
-                  <a title="click to zoom-in "
+                <li
+                  class="gallery-item mix wedevelopment col-lg-4 col-md-6 col-sm-12"
+                >
+                  <a
+                    title="click to zoom-in "
                     href="/images/gallery/works_05.png"
-                    itemprop="contentUrl " data-size="1200x600 ">
+                    itemprop="contentUrl "
+                    data-size="1200x600 "
+                  >
                     <div class="inner-box">
                       <figure class="image">
-                        <img src="/images/gallery/works_05.png" alt=""
-                        ></figure> <a href="/images/gallery/works_05.png" data-fancybox="gallery"
-                        class="lightbox-image overlay-box"></a>
+                        <img src="/images/gallery/works_05.png" alt="" />
+                      </figure>
+                      <a
+                        href="/images/gallery/works_05.png"
+                        data-fancybox="gallery"
+                        class="lightbox-image overlay-box"
+                      ></a>
                       <div class="cap-box">
                         <div class="cap-inner">
                           <div class="cat"><span>Web Development</span></div>
                           <div class="title">
-                            <h5><p class="text-white">
-                              E-Commerce tet</p></h5>
+                            <h5><p class="text-white">E-Commerce tet</p></h5>
                           </div>
                         </div>
                       </div>
@@ -46,15 +64,25 @@
                   </a>
                 </li>
                 <!-- Gallery Item -->
-              <!-- Gallery Item -->
-              <li class="gallery-item mix wedevelopment col-lg-4 col-md-6 col-sm-12">
-                  <a title="click to zoom-in "
+                <!-- Gallery Item -->
+                <li
+                  class="gallery-item mix wedevelopment col-lg-4 col-md-6 col-sm-12"
+                >
+                  <a
+                    title="click to zoom-in "
                     href="/images/gallery/works_03.png"
-                    itemprop="contentUrl " data-size="1200x600 ">
+                    itemprop="contentUrl "
+                    data-size="1200x600 "
+                  >
                     <div class="inner-box">
-                      <figure class="image"><img src="/images/gallery/works_03.png" alt=""
-                        ></figure> <a href="/images/gallery/works_03.png" data-fancybox="gallery"
-                        class="lightbox-image overlay-box"></a>
+                      <figure class="image">
+                        <img src="/images/gallery/works_03.png" alt="" />
+                      </figure>
+                      <a
+                        href="/images/gallery/works_03.png"
+                        data-fancybox="gallery"
+                        class="lightbox-image overlay-box"
+                      ></a>
                       <div class="cap-box">
                         <div class="cap-inner">
                           <div class="cat"><span>Web Developement</span></div>
@@ -66,37 +94,55 @@
                     </div>
                   </a>
                 </li>
-                   <!-- Gallery Item -->
-              <li class="gallery-item mix wedevelopment col-lg-4 col-md-6 col-sm-12">
-                  <a title="click to zoom-in "
+                <!-- Gallery Item -->
+                <li
+                  class="gallery-item mix wedevelopment col-lg-4 col-md-6 col-sm-12"
+                >
+                  <a
+                    title="click to zoom-in "
                     href="/images/gallery/works_04.png"
-                    itemprop="contentUrl " data-size="1200x600 ">
+                    itemprop="contentUrl "
+                    data-size="1200x600 "
+                  >
                     <div class="inner-box">
-                      <figure class="image"><img src="/images/gallery/works_04.png" alt=""
-                        ></figure> <a href="/images/gallery/works_04.png" data-fancybox="gallery"
-                        class="lightbox-image overlay-box"></a>
+                      <figure class="image">
+                        <img src="/images/gallery/works_04.png" alt="" />
+                      </figure>
+                      <a
+                        href="/images/gallery/works_04.png"
+                        data-fancybox="gallery"
+                        class="lightbox-image overlay-box"
+                      ></a>
                       <div class="cap-box">
                         <div class="cap-inner">
                           <div class="cat"><span>Web Developement</span></div>
                           <div class="title">
-                            <h5><p class="text-white">
-                           Entertainment
-                            </p></h5>
+                            <h5><p class="text-white">Entertainment</p></h5>
                           </div>
                         </div>
                       </div>
                     </div>
                   </a>
                 </li>
-                   <!-- Gallery Item -->
-              <li class="gallery-item mix webdesign col-lg-4 col-md-6 col-sm-12">
-                  <a title="click to zoom-in "
+                <!-- Gallery Item -->
+                <li
+                  class="gallery-item mix webdesign col-lg-4 col-md-6 col-sm-12"
+                >
+                  <a
+                    title="click to zoom-in "
                     href="/images/gallery/works_06.png"
-                    itemprop="contentUrl " data-size="1200x600 ">
+                    itemprop="contentUrl "
+                    data-size="1200x600 "
+                  >
                     <div class="inner-box">
-                      <figure class="image"><img src="/images/gallery/works_06.png" alt=""
-                        ></figure> <a href="/images/gallery/works_06.png" data-fancybox="gallery"
-                        class="lightbox-image overlay-box"></a>
+                      <figure class="image">
+                        <img src="/images/gallery/works_06.png" alt="" />
+                      </figure>
+                      <a
+                        href="/images/gallery/works_06.png"
+                        data-fancybox="gallery"
+                        class="lightbox-image overlay-box"
+                      ></a>
                       <div class="cap-box">
                         <div class="cap-inner">
                           <div class="cat"><span>Web Design</span></div>
@@ -108,15 +154,25 @@
                     </div>
                   </a>
                 </li>
-                   <!-- Gallery Item -->
-              <li class="gallery-item mix webapplication col-lg-4 col-md-6 col-sm-12">
-                  <a title="click to zoom-in "
+                <!-- Gallery Item -->
+                <li
+                  class="gallery-item mix webapplication col-lg-4 col-md-6 col-sm-12"
+                >
+                  <a
+                    title="click to zoom-in "
                     href="/images/gallery/works_01.png"
-                    itemprop="contentUrl " data-size="1200x600 ">
+                    itemprop="contentUrl "
+                    data-size="1200x600 "
+                  >
                     <div class="inner-box">
-                      <figure class="image"><img src="/images/gallery/works_01.png" alt=""
-                        ></figure> <a href="/images/gallery/works_01.png" data-fancybox="gallery"
-                        class="lightbox-image overlay-box"></a>
+                      <figure class="image">
+                        <img src="/images/gallery/works_01.png" alt="" />
+                      </figure>
+                      <a
+                        href="/images/gallery/works_01.png"
+                        data-fancybox="gallery"
+                        class="lightbox-image overlay-box"
+                      ></a>
                       <div class="cap-box">
                         <div class="cap-inner">
                           <div class="cat"><span>Web Application</span></div>
@@ -128,15 +184,23 @@
                     </div>
                   </a>
                 </li>
-                   <!-- Gallery Item -->
-              <li class="gallery-item mix ui col-lg-4 col-md-6 col-sm-12">
-                  <a title="click to zoom-in "
+                <!-- Gallery Item -->
+                <li class="gallery-item mix ui col-lg-4 col-md-6 col-sm-12">
+                  <a
+                    title="click to zoom-in "
                     href="/images/gallery/works_02.png"
-                    itemprop="contentUrl " data-size="1200x600 ">
+                    itemprop="contentUrl "
+                    data-size="1200x600 "
+                  >
                     <div class="inner-box">
-                      <figure class="image"><img src="/images/gallery/works_02.png" alt=""
-                        ></figure> <a href="/images/gallery/works_02.png" data-fancybox="gallery"
-                        class="lightbox-image overlay-box"></a>
+                      <figure class="image">
+                        <img src="/images/gallery/works_02.png" alt="" />
+                      </figure>
+                      <a
+                        href="/images/gallery/works_02.png"
+                        data-fancybox="gallery"
+                        class="lightbox-image overlay-box"
+                      ></a>
                       <div class="cap-box">
                         <div class="cap-inner">
                           <div class="cat"><span>UI/UX</span></div>
@@ -148,8 +212,6 @@
                     </div>
                   </a>
                 </li>
-             
-             
               </div>
             </div>
           </ul>
@@ -157,44 +219,51 @@
       </div>
     </section>
   </div>
-  </template>
-  
-  <script>
-  import PageHeader from '../views/PageHeader.vue';
-    export default {
-        components:{PageHeader},
-    name: "GalleryPage",
-    data() {
-        return {
-            mixer: null
-        };
-    },
-      created() {
-        this.$nextTick(() => {
-          var containerEl = document.querySelector("ul.container");
-      var mixer = mixitup(containerEl, {
-        animation: {
-          effects: "fade translateZ(-100px)",
-          effectsIn: "fade translateY(-100%)",
-          easing: "cubic-bezier(0.645, 0.045, 0.355, 1)"
-        }
-      });
+</template>
+
+<script>
+import PageHeader from "../views/PageHeader.vue";
+import Mixitup from "mixitup";
+
+export default {
+  components: { PageHeader },
+  name: "GalleryPage",
+  data() {
+    return {
+      mixer: null,
+    };
+  },
+  mounted(){
+    this.$nextTick(() => {
+      this.initMixitup()
+      this.$refs.all.click();
+    });
+  },
+  created() {
+    this.$nextTick(() => {
       $("[data-fancybox]").fancybox({
-        selector: '.mix:visible a',
+        selector: ".mix:visible a",
         loop: true,
         hash: true,
         transitionEffect: "slide",
         clickContent: function (current, event) {
           return current.type === "image" ? "next" : false;
-        }
+        },
       });
-            });
-     
+    });
+  },
+  methods: {
+    initMixitup() {
+      this.mixitup = Mixitup("ul.container", {
+        animation: {
+          effects: "fade translateZ(-100px)",
+          effectsIn: "fade translateY(-100%)",
+          easing: "cubic-bezier(0.645, 0.045, 0.355, 1)",
+        },
+      });
     },
-}
-  </script>
-  
-  <style scoped>
-  
-  </style>
-  
+  },
+};
+</script>
+
+<style scoped></style>
