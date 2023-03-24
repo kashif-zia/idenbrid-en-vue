36c031ed89d7eba85348b6dbc86a00c123400b12
Muhammad Kashif zia <kashif.idenbrid@gmail.com>
2023-03-24 14:42:55 +0500

    new changes

---
+++ frontend/src/components/Ses.vue
@@ -0,0 +1,164 @@
+<template>
+    <div>
+        <pageHeader title="System Engineering Service" />
+        <main>
+
+            <section class="system-sec auto-container mt-5">
+                <h2 class="system-sec__tiile">Introduction of engineering styles and services for engineers</h2>
+                <p class="system-sec__text">
+                    Engineer Style is our service to support engineers and designers in obtaining projects.<br>
+                    Just register with us and we will help you find a project that meets your requirements.</p>
+                <ul class="system-sec__contents">
+                    <li class="system-sec__list"><img src="/images/icons/ses-image-01.png" alt="Ses image"
+                            class="system-sec__list__thumbnail">
+                        <h3 class="system-sec__list__title">man of style</h3>
+                        <p class="system-sec__list__text">
+                            Style-kun will do its best to assist and support you in obtaining your projects!</p>
+                    </li>
+                    <li class="system-sec__list"><img src="/images/icons/ses-image-02.svg" alt="Ses image"
+                            class="system-sec__list__thumbnail">
+                        <h3 class="system-sec__list__title">Ability to negotiate desired conditions</h3>
+                        <p class="system-sec__list__text">
+                            We offer a working style that meets your requirements so that engineers and designers can
+                            work more comfortably.</p>
+                    </li>
+                    <li class="system-sec__list"><img src="/images/icons/ses-image-03.svg" alt="Ses image"
+                            class="system-sec__list__thumbnail">
+                        <h3 class="system-sec__list__title">Contract unit price</h3>
+                        <p class="system-sec__list__text">We negotiate unit prices and firm rates of return so that we
+                            can return as much as possible to our engineers and designers.
+                        </p>
+                    </li>
+                    <li class="system-sec__list"><img src="/images/icons/ses-image-04.svg" alt="Ses image"
+                            class="system-sec__list__thumbnail">
+                        <h3 class="system-sec__list__title">Full remote case</h3>
+                        <p class="system-sec__list__text">
+                            This is one of the most popular conditions because it provides a more comfortable working
+                            environment for engineers and saves commuting time.</p>
+                    </li>
+                    <li class="system-sec__list"><img src="/images/icons/ses-image-05.svg" alt="Ses image"
+                            class="system-sec__list__thumbnail">
+                        <h3 class="system-sec__list__title">Environment to develop skills</h3>
+                        <p class="system-sec__list__text">
+                            We introduce projects that will not only simply do the work you can do, but also help you
+                            improve your skills in an environment that will challenge you.</p>
+                    </li>
+                    <li class="system-sec__list"><img src="/images/icons/ses-image-06.svg" alt="Ses image"
+                            class="system-sec__list__thumbnail">
+                        <h3 class="system-sec__list__title">Dependable Customer Success</h3>
+                        <p class="system-sec__list__text">In case of trouble, our engineers will be there to help you
+                            understand the situation and dispel any concerns you may have.
+                        </p>
+                    </li>
+                </ul>
+            </section>
+            <section class="system-sec auto-container mt-5">
+                <h2 class="system-sec__tiile">Information for project-side companies in need of human resources</h2>
+                <p class="system-sec__text">
+                    We have people who not only have engineering skills, but also have high communication skills and a
+                    sense of responsibility and commitment to their work. We can also provide a one-stop team structure,
+                    from upstream to downstream.
+                </p>
+                <ul class="system-sec__contents">
+                    <li class="system-sec__list"><img src="/images/icons/ses-image-07.svg" alt="Ses image"
+                            class="system-sec__list__thumbnail">
+                        <h3 class="system-sec__list__title">Extensive human resources database</h3>
+                        <p class="system-sec__list__text">
+                            We are able to propose human resources that cannot be found in other SES companies from the
+                            perspective and network of a company that is also engaged in contracted development.</p>
+                    </li>
+                    <li class="system-sec__list"><img src="/images/icons/ses-image-08.svg" alt="Ses image"
+                            class="system-sec__list__thumbnail">
+                        <h3 class="system-sec__list__title">Flexible proposal capabilities</h3>
+                        <p class="system-sec__list__text">
+                            Even if you have a good candidate, you may not be able to reach an agreement due to
+                            restrictions on commercial distribution, negotiation of terms and conditions, etc. At our
+                            service, we make flexible proposals with a sense of urgency, placing the hiring of good
+                            personnel as our first priority.</p>
+                    </li>
+                    <li class="system-sec__list"><img src="/images/icons/ses-image-09.svg" alt="Ses image"
+                            class="system-sec__list__thumbnail">
+                        <h3 class="system-sec__list__title">speedy response</h3>
+                        <p class="system-sec__list__text">Even if you have an urgent, urgent project or a recruitment
+                            that requires a large number of people, our service will be able to propose and respond to
+                            your needs.
+                        </p>
+                    </li>
+                </ul>
+            </section>
+            <section class="flow">
+                <div class="auto-container">
+                    <h3 class="content-title content-title_flow">Flow of using Engineer Style</h3>
+                    <p class="content-text content-subtitle_flow">
+                        We will explain the flow from registering as a member in our database to actually starting a
+                        job.</p>
+                </div>
+                <div class="flow__scroll auto-container">
+                    <ul class="flow__contents">
+                        <li class="flow__list flow__list_border"><img src="/images/icons/Mobile-login-pana.svg"
+                                alt="Mobile login image" class="flow__list__thumbnail">
+                            <h4 class="flow__list__title">Member Registration for this Service</h4>
+                            <p class="flow__list__text">We will register the personnel in our engineering style database
+                                and begin to make proposals to our clients.</p>
+                        </li>
+                        <li class="flow__list flow__list_border"><img src="/images/icons/Resume-bro.svg"
+                                alt="Resume image" class="flow__list__thumbnail">
+                            <h4 class="flow__list__title">document screening</h4>
+                            <p class="flow__list__text">We will prepare a skill set sheet based on the information you
+                                have registered in order to proceed with your proposal and pass the document screening
+                                process.</p>
+                        </li>
+                        <li class="flow__list flow__list_border"><img src="/images/icons/Consulting-rafiki.svg"
+                                alt="Consulting image" class="flow__list__thumbnail">
+                            <h4 class="flow__list__title">Interviews and adjustment of conditions</h4>
+                            <p class="flow__list__text">
+                                We will meet with you and our clients 1-2 times. We will discuss your past experience in
+                                an interview.</p>
+                        </li>
+                        <li class="flow__list"><img alt="img" src="/images/icons/Inbox-cleanup-pana.svg"
+                                class="flow__list__thumbnail">
+                            <h4 class="flow__list__title">Contract and start of work</h4>
+                            <p class="flow__list__text">Once the contract documents have been securely processed, work
+                                will begin!
+                            </p>
+                        </li>
+                    </ul>
+                </div>
+            </section>
+            <section class="exp-menu-list">
+                <div class="menu-box">
+                    <div class="item"><a href="/development/" class="text"><img alt="img"
+                                src="/images/icons/icons8-pc-on-desk-80.png">Web System Development
+                        </a></div>
+                    <div class="item"><a href="/mobileapp/"
+                            class="text router-link-exact-active router-link-active"><img alt="img"
+                                src="/images/icons/icons8-iphone-14-pro-80.png">Smartphone application development
+                        </a></div>
+                    <div class="item"><a href="/uiuxdesign/" class="text"><img alt="img"
+                                src="/images/icons/icons8-illustrator-80.png">UI/UX design production
+                        </a></div>
+                    <div class="item"><a href="/webitconsulting/" class="text"><img alt="img"
+                                src="/images/icons/icons8-flight-trip-64.png">offshore development</a></div>
+                    <div class="item"><a href="/engineerstyle/" aria-current="page"
+                            class="text router-link-exact-active router-link-active"><img alt="img"
+                                src="/images/icons/color-icon-logo.png">SES Engineer Style</a></div>
+                </div>
+            </section>
+
+        </main>
+    </div>
+</template>
+
+<script>
+    import PageHeader from "../views/PageHeader.vue";
+    export default {
+        components: {
+            PageHeader,
+        },
+        name: "DigitalAssetsPage",
+    };
+</script>
+
+<style>
+   
+</style>
